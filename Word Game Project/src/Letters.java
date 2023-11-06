bc53f20c7a84ba49df829c49e3965649d0cc8f08
amelia.bertagni <amelia.bertagni@MU-C1-14.mullen.pvt.k12.co.us>
2023-11-06 14:39:53 -0700

    initial commit

---
+++ Word Game Project/src/Letters.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+public class Letters
+    {
+    static ArrayList<Words> words = new ArrayList<Words>();
+
+    public static void fillLetters()
+        {
+        words.add(new Words("Juvenile", 3, 15, true));
+        words.add(new Words("Eleven", 3, 6, false));
+        words.add(new Words("Enliven", 3, 7, false));
+        words.add(new Words("Even", 3, 1, false));
+        words.add(new Words("Nill", 3, 1, false));
+        words.add(new Words("Julienne", 3, 8, false));
+        words.add(new Words("Venue", 3, 5, false));
+        words.add(new Words("Neele", 3, 5, false));
+        words.add(new Words("Lien", 3, 1, false));
+        words.add(new Words("Vein", 3, 1, false));
+        words.add(new Words("Line", 3, 1, false));
+        words.add(new Words("Linen", 3, 5, false));
+        words.add(new Words("Vine", 3, 1, false));
+        words.add(new Words("Liven", 3, 5, false));
+        words.add(new Words("Nine", 3, 1, false));
+        words.add(new Words("Lien", 3, 1, false));
+        words.add(new Words("Lune", 3, 1, false));
+        words.add(new Words("Null", 3, 1, false));
+        words.add(new Words("Unlevel", 3, 7, false));
+        words.add(new Words("Unveil", 3, 6, false));
+        words.add(new Words("Venule", 3, 6, false));
+        words.add(new Words("Levin", 3, 5, false));
+        words.add(new Words("Elven", 3, 5, false));
+        words.add(new Words("Venule", 3, 6, false));
+        words.add(new Words("Venene", 3, 6, false));
+        words.add(new Words("Unlive", 3, 6, false));
+        words.add(new Words("Unline", 3, 6, false));
+        words.add(new Words("Uneven", 3, 6, false));
+        words.add(new Words("Inulin", 3, 6, false));
+        
+        words.add(new Words("Pinioning", 2, 9, false));
+        words.add(new Words("Dingdong", 2, 8, false));
+        words.add(new Words("Dripping", 2, 8, false));
+        words.add(new Words("Drooping", 2, 15, false));
+        words.add(new Words("Dropping", 2, 15, false));
+        words.add(new Words("Gridiron", 2, 8, false));
+        words.add(new Words("Grinding", 2, 8, false));
+        words.add(new Words("Grinning", 2, 8, false));
+        words.add(new Words("Gripping", 2, 8, false));
+        words.add(new Words("Ignoring", 2, 8, false));
+        words.add(new Words("Pingpong", 2, 8, false));
+        words.add(new Words("Prodding", 2, 8, false));
+        words.add(new Words("Propping", 2, 8, false));
+        words.add(new Words("Dinging", 2, 7, false));
+        words.add(new Words("Dinning", 2, 7, false));
+        words.add(new Words("Dipping", 2, 7, false));
+        words.add(new Words("Dodging", 2, 7, false));
+        words.add(new Words("Donging", 2, 7, false));
+        words.add(new Words("Donning", 2, 7, false));
+        words.add(new Words("Dopping", 2, 7, false));
+        words.add(new Words("Droning", 2, 7, false));
+        words.add(new Words("Ginning", 2, 7, false));
+        words.add(new Words("Girding", 2, 7, false));
+        words.add(new Words("Griping", 2, 7, false));
+        words.add(new Words("Groping", 2, 7, false));
+        words.add(new Words("Ingoing", 2, 7, false));
+        words.add(new Words("Ironing", 2, 7, false));
+        words.add(new Words("Nipping", 2, 7, false));
+        words.add(new Words("Nodding", 2, 7, false));
+        words.add(new Words("Nooning", 2, 7, false));
+        words.add(new Words("Ongoing", 2, 7, false));
+        words.add(new Words("Opinion", 2, 7, false));
+        words.add(new Words("Pinging", 2, 7, false));
+        words.add(new Words("Pinning", 2, 7, false));
+        words.add(new Words("Pipping", 2, 7, false));
+        words.add(new Words("Podding", 2, 7, false));
+        words.add(new Words("Pogoing", 2, 7, false));
+        words.add(new Words("Ponging", 2, 7, false));
+        words.add(new Words("Pooping", 2, 7, false));
+        words.add(new Words("", 2, , false));
+        
+        words.add(new Words("Quiche", 1, 15, true));
+        words.add(new Words("Cheque", 1, 6, false));
+        words.add(new Words("Queue", 1, 5, false));
+        words.add(new Words("Eche", 1, 1, false));
+        words.add(new Words("Eech", 1, 1, false));
+        }
+   
+    }
---
+++ Word Game Project/src/Words.java
@@ -0,0 +1,56 @@
+public class Words
+	{
+		private String word;
+		private int level;
+		private int points;
+		private boolean panagram;
+		  
+		public Words(String w, int l, int po, boolean p)
+		{
+			word = w;
+			level = l;
+			points = po;
+			panagram = p;
+		}
+		
+		public String getWord()
+		{
+			return word;
+		}
+		
+		public void setWord(String word)
+		{
+			this.word = word;
+		}
+		
+		public int getLevel()
+		{
+			return level;
+		}
+		
+		public void setLevel(int level)
+		{
+			this.level = level;
+		}
+		
+		public int getPoints()
+		{
+			return points;
+		}
+		
+		public void setPoints(int points)
+		{
+			this.points = points;
+		}
+		
+		public boolean getPanagram()
+		{
+			return panagram;
+		}
+		
+		public void setPanagram(int panagram)
+		{
+			this.panagram = panagram;
+		}
+	}
+}
